d4b073df3c38bfc50a47b0ff8657bbc54e19772b
klbk <kjbanaszczyk@gmail.com>
2016-10-20 12:27:42 +0200

    change jdk

---
+++ src/test/java/com/gft/technicalchallenge/CalendarTest.java
@@ -0,0 +1,19 @@
+package com.gft.technicalchallenge;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by klbk on 20/10/2016.
+ */
+public class CalendarTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+
+
+}
